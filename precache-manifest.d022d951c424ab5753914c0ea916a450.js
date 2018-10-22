5c48dbd303deb102b67acdc2f65f77dc24ac6098
JuAn-Kang <doubtful.mr@gmail.com>
2018-10-23 04:24:47 +1100

    Updates

---
+++ precache-manifest.d022d951c424ab5753914c0ea916a450.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "7f548b83d7cdc36dfd3d0d009ce96066",
+    "url": "/forecast-react/static/media/loader.7f548b83.svg"
+  },
+  {
+    "revision": "11aa1e59e9d1f67f10c0",
+    "url": "/forecast-react/static/js/runtime~main.11aa1e59.js"
+  },
+  {
+    "revision": "2205f13524dcde5e7d1c",
+    "url": "/forecast-react/static/js/main.2205f135.chunk.js"
+  },
+  {
+    "revision": "1d2f9d6d07401715b344",
+    "url": "/forecast-react/static/js/1.1d2f9d6d.chunk.js"
+  },
+  {
+    "revision": "2205f13524dcde5e7d1c",
+    "url": "/forecast-react/static/css/main.74273f20.chunk.css"
+  },
+  {
+    "revision": "fbbdead7f4b49f7091f5b0b3cdf6e7d0",
+    "url": "/forecast-react/index.html"
+  }
+];
